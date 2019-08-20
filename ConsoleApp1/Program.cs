bddfad70ba61725b76c08ed8ed01f4d955420e3e
Heath Moore <heathjhmoore@gmail.com>
2019-08-20 11:31:06 -0700

    cool my first c sharp stuff

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            bool myBool = false;
+
+            if (myBool)
+            {
+                Console.WriteLine("we wont hit this code");
+            }
+
+            switch (myBool)
+            {
+                case true:
+                    Console.WriteLine("hey");
+                    break;
+            }
+
+            for (var i=0; i < 5; i++)
+            {
+                Console.WriteLine($"we on iteration {i}");
+            }
+
+            List<string> myStrings = new List<string>();
+
+            foreach(var myString in myStrings)
+            {
+                Console.WriteLine(myString);
+            }
+        }
+    }
+}
