211a3af7164e0e65284ce5ead8e24e3917a3babf
Heath Moore <heathjhmoore@gmail.com>
2019-08-24 11:51:13 -0500

    finished number multiplier

--- Number-Multiplier/Program.cs
+++ Number-Multiplier/Program.cs
@@ -6,7 +6,44 @@ namespace Number_Multiplier
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string userInput = "";
+            string userOption = "";
+            do
+            {
+                Console.WriteLine("Enter a list of integers like the following format: 1,4,6,8. Type exit to quit");
+                userInput = Console.ReadLine();
+                Console.WriteLine("Would you like to square or multiply this series?");
+                userOption = Console.ReadLine();
+                userOption = userOption.ToLower();
+                string[] userNumbers = userInput.Split(",");
+                int multiplytotal = 0;
+                string squaredList = "";
+                foreach (string item in userNumbers)
+                {
+                    if (userOption == "multiply")
+                    {
+                        multiplytotal += int.Parse(item);
+                    }
+                    else
+                    {
+                        squaredList += $"{int.Parse(item) * int.Parse(item)},";
+                    } 
+                }
+                if (userOption == "multiply")
+                {
+                    Console.WriteLine(multiplytotal);
+                }
+                else if (userOption == "square")
+                {
+                    Console.WriteLine(squaredList);
+                }
+                else
+                {
+                    Console.WriteLine("you didnt enter a valid answer");
+                }
+
+            }
+            while (userInput != "exit");
         }
     }
 }
