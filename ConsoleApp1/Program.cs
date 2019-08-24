8475ba5093a86bb066be7a61fa4534e9900ce5bc
Heath Moore <heathjhmoore@gmail.com>
2019-08-24 10:18:48 -0500

    adding homework

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -4,10 +4,39 @@ using System.Collections.Generic;
 namespace ConsoleApp1
 {
     class Program
-    {
+    { 
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Bangazon!");
+            string phrase;
+            do
+            {
+                Console.WriteLine("Please enter a string of letters");
+                phrase = Console.ReadLine();
+                int phraseLength = phrase.Length;
+                int count = 1;
+                string newPhrase = "";
+                foreach (char letter in phrase)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        if (i == 0)
+                        {
+                            newPhrase += Char.ToUpper(letter);
+                        }
+                        else
+                        {
+                            newPhrase += letter;
+                        }
+                    }
+                    if (count != phraseLength)
+                    {
+                        newPhrase += "-";
+                    }
+                    count += 1;
+                }
+                Console.WriteLine($"{newPhrase}");
+            }
+            while (phrase != "exit");
         }
     }
 }
