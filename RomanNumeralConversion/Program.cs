e15878c0990879877a9ba7c138c9d421440c4d7a
Heath Moore <heathjhmoore@gmail.com>
2019-09-15 14:26:07 -0500

    added user interaction to Roman Numeral Console App

--- RomanNumeralConversion/Program.cs
+++ RomanNumeralConversion/Program.cs
@@ -6,7 +6,17 @@ namespace RomanNumeralConversion
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var RomanNumeralConverter = new RomanNumeralConverter();
+            var userInput = "";
+            do
+            {
+                Console.WriteLine("Please Enter a Number between 1 and 3999 to convert to Roman Numeral");
+                Console.WriteLine("Or type exit to leave this program");
+                userInput = Console.ReadLine();
+                var userInputToInteger = int.Parse(userInput);
+                var output = RomanNumeralConverter.Convert(userInputToInteger);
+                Console.WriteLine($"{userInput} converts to {output}");
+            } while (userInput != "exit");
         }
     }
 }
